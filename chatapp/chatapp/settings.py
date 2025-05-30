18dbb24b5aec6933b1ea12419d9707ceeb38bcd4
Selva Praveen <145885087+Selva-Praveen-AC@users.noreply.github.com>
2025-05-30 15:51:43 +0530

    Update settings.py

--- chatapp/chatapp/settings.py
+++ chatapp/chatapp/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
 		'ENGINE': 'django.db.backends.mysql',
 		'NAME': 'chatapp',
 		'USER': 'root',
-		'PASSWORD': 'AgnesClency143',
+		'PASSWORD': '**********',
 		'HOST':'127.0.0.1',
 		'PORT':'3306',
 	}
