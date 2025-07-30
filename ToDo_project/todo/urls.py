ac8bfa0b96ac8aaf202442a499c262fd02a9a5e6
selva praveen <selvapraveen5000@gmail.com>
2025-07-30 09:05:46 +0530

    Login page with mysql

--- ToDo_project/todo/urls.py
+++ ToDo_project/todo/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path("",views.home)
+    path("",views.index),
+    path("register",views.register)
 ]
--- ToDo_project/todo/views.py
+++ ToDo_project/todo/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from .models import Register
 
 
-# Create your views here.
-def home(request):
-    return render(request,"index.html")
+def index(request):
+    return render(request,'index.html')
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        Register.objects.create(username=username,email=email,password=password)
+    return render(request,'register.html',{'username':username, 'email':email, 'password':password})
